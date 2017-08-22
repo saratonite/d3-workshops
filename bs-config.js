fbb21e1cc846c727694a981a0a31a2703dcb646b
Sarath <sarath.hacks@gmail.com>
2017-08-22 17:34:33 +0530

    Added heatmap

--- bs-config.js
+++ bs-config.js
@@ -1,7 +1,11 @@
 
 var rootDir = process.argv[2];
+console.log('',process.argv[2])
 console.info('Serving on ',rootDir);
 var BaseDir = "./src/"+rootDir;
+
+console.log('The base dir :::', BaseDir);
+
 
 // Middle wares
 var browserSync = require('browser-sync');
