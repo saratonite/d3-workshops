0ad4d30f13fa12b1ec10482c7d49e79a0a0b77d4
Sarath <sarath.hacks@gmail.com>
2018-02-04 19:24:09 +0530

    :rainbow: css support

--- src/sample/app.js
+++ src/sample/app.js
@@ -1,7 +1,8 @@
 console.log('Hello d3')
 import * as d3 from 'd3';
-
+import  './style.css';
 const element = d3.select('#chart')
 
 element.style("color","red")
     .style("background","orange")
+    .text('Hello')
